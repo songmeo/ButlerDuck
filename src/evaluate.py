591817ec18a50259531d18278e67edf530ede07f
Song Meo <songmeo@pm.me>
2024-12-27 15:21:22 +0200

    pytest for evaluate

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,8 +1,21 @@
 def evaluate(expression: str) -> str:
-    # todo: catch wrong expression
-    return str(eval(expression))
+    try:
+        ans = eval(expression)
+    except Exception as error:
+        return str(type(error).__name__)
+    return str(ans)
 
 
 def test_evaluate() -> None:
     # TODO: add PyTest config to the project
     assert evaluate("123 + 456") == str(123 + 456)
+
+
+def test_evaluate_name_error() -> None:
+    # TODO: add PyTest config to the project
+    assert evaluate("455 +_/ 342") == "NameError"
+
+
+def test_evaluate_syntax_error() -> None:
+    # TODO: add PyTest config to the project
+    assert evaluate("455 +_( 342") == "SyntaxError"
